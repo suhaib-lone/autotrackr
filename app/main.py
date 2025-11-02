c8466abd5bb826a564c1be48027304673faa5c11
suhaib-lone <suhaibsareer09@gmail.com>
2025-11-02 17:09:26 +0530

    fixed vulnerabilitites

--- app/main.py
+++ app/main.py
@@ -11,10 +11,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173",
-        "http://localhost:8080", 
-        "http://127.0.0.1:5173",
-        "http://127.0.0.1:8080",
+        "*"
     ],
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
